28717053c5746faa15081c9930f567ffec45ffc3
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-11-03 18:29:02 +0100

    zadatak-6

---
+++ zadatak_6.py
@@ -0,0 +1,16 @@
+# 6. ANALIZIRAJ SLJEDEĆE FOR PETLJE
+
+# 6.1. Pojasnite zašto sljedeća petlja nema (previše) smisla
+for i in range(1,2):
+    print(i)
+# jer će ispisati samo 1, odnosno izvršava se jednom
+
+# 6.2. Što će ispisati sljedeća petlja?
+for i in range(1, 10, 2):
+    print(i)
+# 1 3 5 7 9
+
+# 6.3. Što će ispisati sljedeća petlja?
+for i in range(10, 1, -1):
+    print(i)
+# 10 9 8 7 6 5 4 3 2 
